ca75d2fdc931da62dbc1da0140d6f6ec796054fe
TiWo2012 <timo.wolf@gmx.ch>
2025-01-23 16:56:01 +0100

    nicer formating for index.html, refactored the web.rs file into a differnt function

--- src/web.rs
+++ src/web.rs
@@ -6,20 +6,11 @@ fn read_file(path: &str) -> Result<String, std::io::Error> {
     fs::read_to_string(path)
 }
 
-pub fn handle_client(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
-    
-    // Read the request from the client
-    if let Err(e) = stream.read(&mut buffer) {
-        eprintln!("Failed to read from stream: {}", e);
-        return;
-    }
-
-    // Determine the requested resource
-    let request = String::from_utf8_lossy(&buffer);
+#[allow(unused_assignments)]
+fn handle_requests(request: String) -> String {
     let mut response = String::new();
     let content_type: &str;
-
+    
     if request.starts_with("GET /index.html") || request.starts_with("GET /") {
         content_type = "text/html";
         let html_content = read_file("src/index.html").unwrap_or_else(|e| {
@@ -38,8 +29,23 @@ pub fn handle_client(mut stream: TcpStream) {
         response = "HTTP/1.1 404 Not Found\r\n\r\nResource not found.".to_string();
     }
 
+    return response;
+}
+
+pub fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    
+    // Read the request from the client
+    if let Err(e) = stream.read(&mut buffer) {
+        eprintln!("Failed to read from stream: {}", e);
+        return;
+    }
+
+    // Determine the requested resource
+    let request = String::from_utf8_lossy(&buffer);
+
     // Write the Response back to the client
-    if let Err(e) = stream.write_all(response.as_bytes()) {
+    if let Err(e) = stream.write_all(handle_requests(request.to_string()).as_bytes()) {
         eprintln!("Failed to write to stream: {}", e);
     }
 }
